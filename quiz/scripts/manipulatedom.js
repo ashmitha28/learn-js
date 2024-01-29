1316fe7577c65e9acb74efef104eb116ee59a3c2
Ashmitha <70202007+ashmitha28@users.noreply.github.com>
2024-01-29 16:15:49 -0500

    class

--- quiz/scripts/manipulatedom.js
+++ quiz/scripts/manipulatedom.js
@@ -20,12 +20,28 @@ function createTxtNode(txt) {
   let txtNode = document.createTextNode(txt);
   return txtNode;
 }
+function createBtnNode(btnText,e,l){
+  let btnNode = document.createElement("button");
+  let btnNodeText = createTxtNode(btnText);
+  btnNode.appendChild(btnText);
+  btnNode.addEventListener(e,l);
+  return btnNode;
+}
 
 function addTable() {
   const tableNode = document.createElement("table");
   for(let i = 0; i < 3; i++) {
     let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
+    let col2 = createTDNode(createBtnNode("edit text","click",function(){
+      encodeURIComponent(col1);
+    }))
     tableNode.appendChild(createTRNode([col1]));
   }
   document.getElementById("root").appendChild(tableNode);
+}
+function edit(node){
+  let newNode = document.createElement("input");
+  newNode.type='text';
+  newNode.placeholder='enter cell (x,y)...';
+  node.placeholder(newNode,newNode.replaceChild,)
 }
